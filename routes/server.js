d36bfca27c249cd14b88955b4412f548846587be
IRLANDEC0151 <katkov.syy@gmail.com>
2021-08-15 22:01:54 +0300

    pup

--- routes/server.js
+++ routes/server.js
@@ -2,9 +2,6 @@ const e = require("express");
 const { Router } = require("express");
 const router = Router();
 const puppeteer = require("puppeteer")
-var axios = require('axios');
-
-
 
 
 router.get('/', async (req, res) => {
@@ -22,77 +19,66 @@ router.post('/postForm', async (req, res) => {
 
 
 async function parsing(param) {
-    let arr = []
-    let items = 0
-    const link = 'https://fedresurs.ru/backend/encumbrances/'
+    const link = 'https://fedresurs.ru/search/encumbrances'
+    let browser;
+    let date = param.dateFrom.split('-').reverse().join('') + param.dateTo.split('-').reverse().join('')
+
     try {
-        let configToFound = {
-            method: 'get',
-            url: 'https://fedresurs.ru/backend/encumbrances?offset=15&limit=15&searchString=7709431786&publishDateStart=2021-08-12T00:00:00.000&publishDateEnd=2021-08-14T23:59:59.999',
-            headers: {
-                'Referer': `https://fedresurs.ru/search/encumbrances?offset=0&limit=15&searchString=7709431786&additionalSearchFnp=true&publishDateStart=2021-08-12T00:00:00.000&publishDateEnd=2021-08-14T23:59:59.999`,
-                'Cookie': 'fedresurscookie=18b095deae8df78427a4c1569de7322b',
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36'
-            },
-            // params: {
-            //     offset: 0,
-            //     limit: 15,
-            //     searchString: param.inn,
-            //     publishDateStart: `${param.dateFrom}T00:00:00.000`,
-            //     publishDateEnd: `${param.dateTo}T23:59:59.999`
-            // },
-        }; 
-        await axios(configToFound)
-            .then((res) => {
-                console.log(res);
-                items = res.data.found + 15
-                console.log('items: ', items);
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
-        let count = 0
-        while (count < items) {
-            console.log(`count=${count}`);
-            let config = {
-                method: 'get',
-                url: link,
-                headers: {
-                    'referer': `https://fedresurs.ru/search/encumbrances?offset=${count}&limit=15&searchString=7709431786&additionalSearchFnp=true&publishDateStart=2021-08-13T00:00:00.000&publishDateEnd=2021-09-01T23:59:59.999`,
-                    'Cookie': 'fedresurscookie=18b095deae8df78427a4c1569de7322b',
-                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36'
-                 },
-                params: { 
-                    offset: count, 
-                    limit: 15,
-                    searchString: 7709431786,
-                    publishDateStart: '2021-08-13T00:00:00.000',
-                    publishDateEnd: '2021-09-01T23:59:59.999'
-                },
-            };
-
-            await axios(config)
-                .then(function (res) {
-                    let dataArr = res.data.pageData.map(item => {
-                        let parseString = res.data.pageData[0].mainInfo.split('Лизингополучатель: ')[res.data.pageData[0].mainInfo.split('Лизингополучатель: ').length - 1].split(',')
-                        let inn = parseString[parseString.length - 1].replace(/[^0-9]/g, "")
-                        let nameCompany = parseString[0]
-
-                        return { inn, nameCompany }
-                    })
-                    dataArr = dataArr.filter((item, index, array) => array.findIndex(i => (i.inn === item.inn)) === index)
-                    arr = arr.concat([...new Set(dataArr)]);
-                })
-                .catch(function (error) {
-                    console.log(error);
-                });
-            count += 15
+        browser = await puppeteer.launch({
+            headless: true,
+            args: [
+                '--no-sandbox',
+                '--disable-setuid-sandbox',
+              ],  
+        })
+        const page = await browser.newPage();
+        await page.goto(link, { waitUntil: "domcontentloaded" })
+        //набираю Инн  
+        console.log(await page.content());
+        await page.waitForSelector('.form-control');
+        await page.click('.form-control');
+        await page.type('.form-control', `${param.inn}`);
+        console.log('a тут');
+        //набираю дату
+        await page.click('body > fedresurs-app > div:nth-child(3) > search > div > div > div > encumbrances-search > div > div > div > form > expand-panel > div.toggle > a')
+        await page.waitForSelector('#id1 > div > div.form-group.hide_label.load_publ_date > date-range-picker > div > div > input.datepicker-range-wrap__input-field.datepicker-range-wrap__input-field_from.ng-untouched.ng-pristine.ng-valid', { visible: true })
+        //с-по
+        //await page.click('#id1 > div > div.form-group.hide_label.load_publ_date > date-range-picker > div > div > input.datepicker-range-wrap__input-field.datepicker-range-wrap__input-field_from.ng-untouched.ng-pristine.ng-valid')
+        await page.type('#id1 > div > div.form-group.hide_label.load_publ_date > date-range-picker > div > div > input.datepicker-range-wrap__input-field.datepicker-range-wrap__input-field_from.ng-untouched.ng-pristine.ng-valid', date);
+        //поиск
+        await page.click('body > fedresurs-app > div:nth-child(3) > search > div > div > div > encumbrances-search > div > div > div > form > button')
+        await page.waitForSelector('.all_biddings', { visible: true })
+        let count = await page.evaluate(() => {
+            return Math.ceil(parseInt(document.querySelector('.search-count-block').textContent) / 15 - 1)
+        })
+        while (count != 1) {
+            await page.click('.btn_load_more')
+            await page.waitForSelector('.btn_load_more');
+            count--
         }
-        console.log(arr.length);
-        return arr;
+
+        let html = await page.evaluate(() => {
+            let data = []
+            let container = document.querySelector('.all_biddings').children
+            let tr = Array.prototype.slice.call(container);
+            for (let index = 0; index < tr.length; index++) {
+                const element = tr[index];
+                let text = element.children[1].children[1].textContent
+                let parseString = text.split('Лизингополучатель: ')[text.split('Лизингополучатель: ').length - 1].split(',')
+                let inn = parseString[parseString.length - 1].replace(/[^0-9]/g, "")
+                let nameCompany = parseString[0]
+                data.push({
+                    inn,
+                    nameCompany
+                })
+            }
+            return data
+        })
+        html = html.filter((item, index, array) => array.findIndex(i => (i.inn === item.inn)) === index)
+
+        return html
     } catch (error) {
         console.log(error);
     }
 }
-
 module.exports = router;
