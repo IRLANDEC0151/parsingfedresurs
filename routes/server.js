d5d753b9ce8fe3dff14ed03b75067d30314f77d6
IRLANDEC0151 <katkov.syy@gmail.com>
2021-08-15 12:18:19 +0300

    postman

--- routes/server.js
+++ routes/server.js
@@ -23,7 +23,7 @@ router.post('/postForm', async (req, res) => {
 
 async function parsing(param) {
     let arr = []
-    let items=0
+    let items = 0
     const link = 'https://fedresurs.ru/backend/encumbrances/'
     try {
         let configToFound = {
@@ -33,14 +33,15 @@ async function parsing(param) {
                 'referer': `https://fedresurs.ru/search/encumbrances?offset=0&limit=15&searchString=${param.inn}T00:00:00.000&additionalSearchFnp=true&publishDateStart=${param.dateFrom}&publishDateEnd=${param.dateto}T23:59:59.999`,
                 'Accept-Encoding': 'gzip, deflate, br',
                 'Connection': 'keep-alive',
-                'Cookie': 'fedresurscookie=8678c7cca03457ecde68163a7f433df2'
+                'Cookie': 'fedresurscookie=8678c7cca03457ecde68163a7f433df2',
+                'User-Agent': 'PostmanRuntime/7.28.3'
             },
             params: {
                 offset: 0,
                 limit: 15,
                 searchString: param.inn,
                 publishDateStart: `${param.dateFrom}T00:00:00.000`,
-                publishDateEnd: `${param.dateTo}T23:59:59.999`,
+                publishDateEnd: `${param.dateTo}T23:59:59.999`
             },
         };
         await axios(configToFound)
